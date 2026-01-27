024beb4e3af63aa04fbe777accc1b8adcb421ec0
marcusleonas <marcus@marcusleonasharvey.com>
2026-01-27 11:45:49 +0000

    initial commit

---
+++ src/data/buttons.ts
@@ -0,0 +1,25 @@
+interface Button { src: string; href?: string };
+
+export const buttons: Button[] = [
+    {
+        src: "/images/buttons/Censor_14c.gif"
+    },
+    {
+        src: "/images/buttons/gay.png",
+    }
+]
+
+export const extButtons: Button[] = [
+    {
+        src: "https://lazer-bunny.neocities.org/Art_Storage/Pixel_Art/Button_88x31.gif",
+        href: "https://lazer-bunny.neocities.org",
+    },
+    {
+        src: "https://file.garden/aNYsXwv8qTXml6YP/playpenz%20button.png",
+        href: "https://playpenz.party",
+    },
+    {
+        src: "/images/buttons/cozyghosty25.png",
+        href: "https://cozyghostly.neocities.org/link",
+    },
+]
