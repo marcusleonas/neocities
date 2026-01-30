da5f352cd7f78801e690560745d1ede8e91695eb
marcusleonas <marcus@marcusleonasharvey.com>
2026-01-30 11:00:01 +0000

    add buttons

--- src/data/buttons.ts
+++ src/data/buttons.ts
@@ -15,4 +15,29 @@ export const buttons: Button[] = [
         href: "http://lockheart.love",
         category: "neighbour",
     },
+    {
+        src: "/images/buttons/cozyghosty25.png",
+        href: "https://cozyghostly.neocities.org",
+        category: "liked",
+    },
+    {
+        src: "/images/buttons/lazerbunny.gif",
+        href: "https://lazer-bunny.neocities.org",
+        category: "liked",
+    },
+    {
+        src: "https://raw.githubusercontent.com/bechnokid/neocities/refs/heads/master/public/assets/images/button.png",
+        href: "https://bechnokid.neocities.org",
+        category: "liked",
+    },
+    {
+        src: "https://file.garden/Z38yn5JN2nXAbi-Q/site/ezgif.com-animated-gif-maker.gif",
+        href: "https://fishshrine.neocities.org",
+        category: "liked",
+    },
+    {
+        src: "/images/buttons/makoenergy.gif",
+        href: "https://makoenergy.neocities.org",
+        category: "liked",
+    },
 ];
